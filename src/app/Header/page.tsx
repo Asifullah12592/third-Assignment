d89cfa5f57ab505e7621284f5e3dfe3b373d5a7a
Asifullah12592 <Asifullah12592@gmail.com>
2024-11-28 10:24:51 +0500

    done

--- src/app/Header/page.tsx
+++ src/app/Header/page.tsx
@@ -1,16 +1,14 @@
 import Link from "next/link"
 
+export default function Header() {
+    return (
 
-
-export default function Header(){
-return(
-    
-    <div className="header">
-<ul>
- <li><Link href="/">Home</Link></li>
- <li><Link href="./About">About</Link></li>
- <li><Link href="./Contact-us">Contact-Us</Link></li>
-</ul>
-</div>
+        <div className="header">
+            <ul>
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="./About">About</Link></li>
+                <li><Link href="./Contact-us">Contact-Us</Link></li>
+            </ul>
+        </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,14 @@
-
-
-export default function Home(){
-  return(
+export default function Home() {
+  return (
     <div>
 
-  <div className="content">
-       <div className="textArea">
-                <h1>Hi, I'm <span>Hafiz Asifullah</span><br />
-                    Frontend Developer</h1>
-       </div>
-        
-      
-            <div className="img"><img src='./Mileston-2-Next.js.jpg' alt="img address copied" /></div>
-
-  </div>
-
+      <div className="content">
+        <div className="textArea">
+          <h1>Hi I am <span>Hafiz Asifullah</span><br />
+            Frontend Developer</h1>
+        </div>
+        <div className="img"><img src='./Mileston-2-Next.js.jpg' alt="img address copied" ></img></div>
+      </div>
     </div>
   )
 }
@@ -22,5 +16,9 @@ export default function Home(){
 
 
 
-    
-    
+
+
+
+
+
+
